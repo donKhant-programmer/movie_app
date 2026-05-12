56095623b1de62a1ae175a281c4bf671b1bfaf80
Don Khant <donkhant1@gmail.com>
2026-05-12 14:58:19 +0630

    Initial Flutter project setup

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movie App',
+      theme: ThemeData.dark(),
+      home: const Scaffold(body: Center(child: Text('Movie App'))),
+    );
+  }
+}
